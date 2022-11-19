00f5c9c27f668a71637d563330c29ae3ca9c3469
Андрей Полянский <andreudasug@gmail.com>
2022-11-19 16:12:43 +0500

    1

---
+++ Полянский_Логинов_ПЗ_5/TestProject/TestLibrary/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    internal class TestClass
+    {
+        double x = 30;
+        double y= 2;
+        public void information()
+        {
+            Console.WriteLine($"x = {x}\ty = {y}");
+            Console.WriteLine($"результат = {stepen(x, y)}");
+        }
+        public double stepen(double x, double y)
+        {
+            double result = Math.Pow(x, y);
+            return result;
+        }
+    }
+}
